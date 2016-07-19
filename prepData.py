02c0d17a5f90b300b4d37841ae8ba649a26fb8ce
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-07-19 16:57:46 -0500

    process PBP data

--- prepData.py
+++ prepData.py
@@ -5,6 +5,7 @@ from pprint import pprint
 
 pbp_data = []
 moments_data = []
+players = []
 
 # 1. Import data specified by a the game number (CL argument)
 def importData(game_id):
@@ -30,15 +31,74 @@ def createPlayersDict():
 def processPlayByPlayData():
     plays = []
     nba_date = moments_data['gamedate']
-    game_time = pbp_data['resultSets'][0]['rowSet'][0][5]
-    game_time_hour = int(game_time[:game_time.find(':')])
-    if 'PM' in game_time:
-        game_time_hour += 12
-    game_time_minute = int(game_time[game_time.find(':') + 1:game_time.find(' ')])
+    start_time = calculateTimeFromNBATimeString(pbp_data['resultSets'][0]['rowSet'][0][5])
 
-    date = datetime.datetime(int(nba_date[0:4]), int(nba_date[5:7]), int(nba_date[8:10]), game_time_hour, game_time_minute)
-    game_start_unix = date.timestamp()
+    date = datetime.datetime(int(nba_date[0:4]), int(nba_date[5:7]), int(nba_date[8:10]), start_time['hour'], start_time['minute'])
+    game_start_timestamp = date.timestamp()
 
+    for nba_play in pbp_data['resultSets'][0]['rowSet']:
+        # generate timestamp
+        play_time = calculateTimeFromNBATimeString(nba_play[5])
+        play_time['seconds'] = 60 - int(nba_play[6][-2]) - 1
+        timestamp = datetime.datetime(int(nba_date[0:4]), int(nba_date[5:7]), int(nba_date[8:10]), play_time['hour'], play_time['minute'], play_time['seconds']).timestamp()
+
+        # parse the various playstrings
+        wdw = whoDidWhat(nba_play[7:10], nba_play[1]) or {}
+
+        play = {
+            'eventnum': nba_play[1],
+            'eventmsgtype': nba_play[2],
+            'eventmsgactiontype': nba_play[3],
+            'period': nba_play[4],
+            'timestamp': int(timestamp),
+            **wdw
+        }
+
+        plays.append(play)
+
+def calculateTimeFromNBATimeString(timestring):
+    hour = int(timestring[:timestring.find(':')])
+    if 'PM' in timestring:
+        hour += 12
+
+    minute = int(timestring[timestring.find(':') + 1:timestring.find(' ')])
+    return {'hour': hour, 'minute': minute}
+
+def whoDidWhat(details, play_type):
+    home_play = parsePlay(details[0], 'home') or {}
+    nuetral_play = parsePlay(details[1], 'neutral') or {}
+    visitor_play = parsePlay(details[2], 'visitor') or {}
+
+    merged = {**home_play, **nuetral_play, **visitor_play, 'play_type': play_type}
+    return merged
+
+def parsePlay(playstring, location):
+    if not playstring:
+        return;
+
+    playstring_player_id = 0
+
+    # TODO - extend with various nicknames
+    for player in players['home'] + players['visitor']:
+        if player['lastname'] in playstring:
+            playstring_player_id = player['playerid']
+
+    offensivePlays = ['Dunk', 'Jump Shot', 'Bank Shot', 'Dunk Shot', 'Layup', 'Jumper', 'Hook', '3PT', 'MISS', 'Turnover', 'Free Throw', 'Offensive Charge Foul']
+    defensivePlays = ['STEAL', 'BLOCK', 'L.B.FOUL', 'P.FOUL', 'S.FOUL', 'Shooting Block Foul', 'Personal Block Foul', 'T.Foul', 'Personal Take Foul']
+
+    if any(oplay in playstring for oplay in offensivePlays):
+        side = 'off'
+    elif any(dplay in playstring for dplay in defensivePlays):
+        side = 'def'
+    else:
+        side = 'nue-' + location
+
+    return {
+        side: {
+            'player_id': playstring_player_id,
+            'playstring': playstring
+        }
+    }
 
 # 2. Clean moment data
     # a. Flatten events/moments to a single array of frames
@@ -71,9 +131,10 @@ def main():
 
     game_id = sys.argv[1]
     importData(game_id)
+    global players
     players = createPlayersDict()
     processPlayByPlayData()
-    processMomentsData()
+    # processMomentsData()
 
 if __name__ == '__main__':
     main()
