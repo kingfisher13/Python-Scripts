39da5217879305d9efb8af29ac3642949b83f7d6
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-07-25 15:56:18 -0500

    Initial file

---
+++ defensiveAssignments.py
@@ -0,0 +1,10 @@
+import sys
+import json
+
+def main():
+    if len(sys.argv) != 2:
+        print("usage: 123123 where 123123 is the game_id")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
