c76775b924836ff5cde3cb4ff6fac63fd10f1a14
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-07-26 16:01:18 -0500

    Get offensive plusMinus

--- defPlusMinus.py
+++ defPlusMinus.py
@@ -14,7 +14,27 @@ def importData(game_id, pairing_alg):
         global data
         data = json.load(processed_json_file)
 
+def processData():
+    """ Calculates the Plus Minus for each player for the game """
+    home_players = data['players']['home']
+    visitor_players = data['players']['visitor']
 
+    for play in data['gameData']:
+        if len(play[6]) != 0 and 'off' in play[6] and 'pts' in play[6]['off']:
+            off = play[6]['off']
+            pts = int(play[6]['off']['pts'])
+
+            player = next((p for p in home_players + visitor_players if p['playerid'] == off['player_id']), None)
+            if 'plusMinus' not in player:
+                player['plusMinus'] = 0
+
+            # offensive plus
+            player['plusMinus'] += pts
+
+            print(play)
+            # print(player)
+
+    return { data['teams']['home']['abbreviation']: home_players,  data['teams']['visitor']['abbreviation']: visitor_players}
 
 def main():
     if len(sys.argv) != 3:
@@ -25,6 +45,8 @@ def main():
     game_id = sys.argv[1]
     pairing_alg = sys.argv[2]
     importData(game_id, pairing_alg)
+
+    processed_data = processData()
 
 if __name__ == '__main__':
     main()
