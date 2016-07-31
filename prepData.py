e0efa50eec205c6f0b21927f8a0f1210be1d1d14
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-07-31 15:39:01 -0500

    Use relative paths based on execution directory

--- prepData.py
+++ prepData.py
@@ -1,26 +1,18 @@
 import sys, json, os, glob
 
-def getIdsFromGlob(path):
-    # absolute paths
-    ids = glob.glob(path + '/*.json')
-
+def getIdsFromGlob():
+    """ Returns a list of json files in the current directory """
     # relative paths
-    # ids = glob.glob('*.json')
+    ids = glob.glob('*.json')
     return ids
-
-def getScriptPath():
-    """ Gets the directory where the script was executed, even if __file__ doesn't exist """
-    return os.path.dirname(os.path.realpath(sys.argv[0]))
 
 def main():
     if len(sys.argv) != 1:
         print("usage: no arguments, call in folder with moments data")
         sys.exit(1)
 
-    directory = getScriptPath()
-
     # get list of all files
-    ids = getIdsFromGlob(directory)
+    ids = getIdsFromGlob()
     print(ids)
 
 if __name__ == '__main__':
