d297f99f82df39994972de29a748e290f6f365c2
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-07-31 22:24:47 -0500

    Get pbp data from nba.com and export to json

--- prepData.py
+++ prepData.py
@@ -1,4 +1,5 @@
 import sys, json, os, glob
+import requests
 
 def getIdsFromGlob():
     """ Returns a list of gameids from json files in the current directory """
@@ -8,6 +9,16 @@ def getIdsFromGlob():
     ids = list(map(lambda x: x[:-5], ids))
     return ids
 
+def getPbpDataFromNBASite(game_id):
+    """ Download Play by play data from Nba.com and export to JSON """
+    url = 'http://stats.nba.com/stats/playbyplay?GameID=' + game_id + '&RangeType=0&StartPeriod=0&EndPeriod=0&StartRange=0&EndRange=0'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+    headers = { 'user-agent' : user_agent }
+    r = requests.get(url, headers=headers)
+    print('Downloading ' + game_id)
+    with open(game_id + '-pbp.json', 'w') as export:
+        json.dump(r.json(), export)
+
 def main():
     if len(sys.argv) != 1:
         print("usage: no arguments, call in folder with moments data")
@@ -15,7 +26,10 @@ def main():
 
     # get list of all files
     ids = getIdsFromGlob()
-    print(ids)
+
+    # get pbp data
+    for i in ids:
+        getPbpDataFromNBASite(i)
 
 if __name__ == '__main__':
     main()
