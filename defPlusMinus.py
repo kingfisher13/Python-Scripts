31ad76f77ed380ece79d532c720cef8e3b7d37fd
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-08-02 21:27:16 -0500

    Generate averages/min/max

--- defPlusMinus.py
+++ defPlusMinus.py
@@ -62,6 +62,47 @@ def export(processed_data, game_id, pairing_alg):
     with open('defPlusMinus.' + pairing_alg + '/' + game_id + '.json', 'w') as export:
         json.dump(processed_data, export)
 
+def generateAggregates(all_data):
+    """ Aggregates the plus/minus into a nice json array  """
+    aggregate = {}
+    for game in all_data:
+        for team in all_data[game]:
+            for player in all_data[game][team]:
+                player['team'] = team
+                if 'plusMinus' not in player:
+                    continue
+
+                if player['playerid'] in aggregate:
+                    aggregate[player['playerid']]['aggPlusMinus'].append(player['plusMinus'])
+                else:
+                    aggregate[player['playerid']] = {
+                        'playerid': player['playerid'],
+                        'position': player['position'],
+                        'lastname': player['lastname'],
+                        'jersey': player['jersey'],
+                        'firstname': player['firstname'],
+                        'aggPlusMinus': [player['plusMinus']]
+                    }
+    return aggregate
+
+def calculateStats(agg):
+    """ Calculates averages/highs/lows from all the data """
+    for pid in agg:
+        player = agg[pid]
+        if len(player['aggPlusMinus']) > 0:
+            player['stats'] = {
+                'average': sum(player['aggPlusMinus']) / len(player['aggPlusMinus']),
+                'max': max(player['aggPlusMinus']),
+                'min': min(player['aggPlusMinus'])
+            }
+
+    return agg
+
+def exportAggregate(agg, pairing_alg):
+    """ Exports the data into a single json file on disk """
+    with open('defPlusMinus.' + pairing_alg + '/aggregate.json', 'w') as export:
+        json.dump(agg, export)
+
 def main():
     if len(sys.argv) < 2 or len(sys.argv) > 3:
         print("usage: pairing-algorithm pairings-folder(optional)")
@@ -88,13 +129,20 @@ def main():
     print(ids)
 
     # Go through game by game and generate def-plus-minus
+    all_data = {}
     for i in ids:
         # import data based on game_id
         importData(i, pairing_alg, pairings_dir)
 
         processed_data = processData()
+        all_data[i] = processed_data
         export(processed_data, i, pairing_alg)
         print('Finished generating defensive plus/minus for: ' + i)
+
+    print('Generating aggregates')
+    agg = generateAggregates(all_data)
+    agg = calculateStats(agg)
+    exportAggregate(agg, pairing_alg)
 
 if __name__ == '__main__':
     main()
